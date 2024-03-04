81d065e82c6ba5738dfad3f1303d139f9416f969
Aniikkk <anikethanrao1414@gmail.com>
2024-03-04 15:33:58 +0530

    ssl

--- ma-client.py
+++ ma-client.py
@@ -1,23 +1,14 @@
-import socket             
+import socket
 import struct
 import time
 import json
 from tkinter import *
- 
-# Create a set object 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- 
-# Define the port on which you want to connect 
-port = 3423               
- 
-s.connect(('127.0.0.1', port)) 
+import ssl
 
 def send_msg(sock, msg):
     # Prefix each message with a 4-byte length (network byte order)
     msg = struct.pack('>I', len(msg)) + msg
     sock.sendall(msg)
-
-
 
 def recvall(sock, n):
     # Helper function to recv n bytes or return None if EOF is hit
@@ -38,13 +29,30 @@ def recv_msg(sock):
     # Read the message data
     return recvall(sock, msglen)
 
+# Create a set object 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# SSL context creation
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+context.load_verify_locations(cafile="/Users/windows/projects/Socket-Maze-Game/server.crt") 
+context.check_hostname = False  # Disable hostname verification
+
+# Wrap the socket with SSL
+ssl_sock = context.wrap_socket(s, server_hostname='127.0.0.1')
+
+# Define the port on which you want to connect 
+port = 3423               
+
+# Connect to the server
+ssl_sock.connect(('127.0.0.1', port))
+
 def create():
     "Create a rectangle with draw function (below) with random color"
-    for row in range(ms):
-        for col in range(ms):
-            if map[row][col] == 'P':
+    for row in range(maze_size):
+        for col in range(maze_size):
+            if maze[row][col] == 'P':
                 color = 'White'
-            elif map[row][col] == 'w':
+            elif maze[row][col] == 'w':
                 color = 'black'
             draw(row, col, color)
 
@@ -55,26 +63,26 @@ def draw(row, col, color):
     y2 = y1 + cell_size
     ffs.create_rectangle(x1, y1, x2, y2, fill=color)
 
-received = recv_msg(s)
+received = recv_msg(ssl_sock)
 m = received.decode("utf-8")
-msg= json.loads(m)
+msg = json.loads(m)
 
-ms =msg["ms"]
-map =msg["map"]
-cell_size=msg["cell_size"]
-scr=msg["scr"]
-scc=msg["scc"]
-start_color=msg["start_color"]
-ecr=msg["ecr"]
-ecc=msg["ecc"]
-end_color=msg["end_color"]
-canvas_size=msg["canvas_size"]
-x1=msg["x1"]
-y1=msg["y1"]
+maze_size = msg["ms"]
+maze = msg["map"]
+cell_size = msg["cell_size"]
+scr = msg["scr"]
+scc = msg["scc"]
+start_color = msg["start_color"]
+ecr = msg["ecr"]
+ecc = msg["ecc"]
+end_color = msg["end_color"]
+canvas_size = msg["canvas_size"]
+x1 = msg["x1"]
+y1 = msg["y1"]
 
 window = Tk()
 window.title('Maze')
-ffs = Canvas(window, width = canvas_size, height = canvas_size, bg = 'grey')
+ffs = Canvas(window, width=canvas_size, height=canvas_size, bg='grey')
 ffs.pack()
 
 create()
@@ -92,41 +100,40 @@ def del_rect():
 def move(event):
     global x1, y1
     del_rect()
-    col = w = x1//cell_size
-    row = h = y1//cell_size
+    col = w = x1 // cell_size
+    row = h = y1 // cell_size
     if event.char == "a":
-        if map[row][col - 1] == "P":
+        if maze[row][col - 1] == "P":
             x1 -= cell_size
     elif event.char == "d":
-        if map[row][col + 1] == "P":
+        if maze[row][col + 1] == "P":
             x1 += cell_size
     elif event.char == "w":
-        if map[row - 1][col] == "P":
+        if maze[row - 1][col] == "P":
             y1 -= cell_size
     elif event.char == "s":
-        if map[row + 1][col] == "P":
+        if maze[row + 1][col] == "P":
             y1 += cell_size
 
     draw_rect()
-    col = w = x1//cell_size
-    row = h = y1//cell_size
+    col = w = x1 // cell_size
+    row = h = y1 // cell_size
     if(w == ecc and h == ecr):
         print("GameOver")
         total_time = round((time.time() - start_time), 2)
-        print("Time taken:",total_time)
+        print("Time taken:", total_time)
 
         timeTaken = {"total_time": total_time}
         jsonObj = json.dumps(timeTaken, indent=2).encode("utf-8")
 
         data = jsonObj
 
-        send_msg(s, data)
+        send_msg(ssl_sock, data)
         window.destroy()
 
 def cl():
-    s.close()
+    ssl_sock.close()
 
 window.bind("<Key>", move)
-
 
 window.mainloop()
--- ma-serve.py
+++ ma-serve.py
@@ -1,38 +1,44 @@
-import socket			 
-from random import randint
-import json
+import socket
 import struct
+import time
+import json
+import ssl
+from random import randint
 
-cell_size = 12 #pixels
-ms = 10 # rows and columns
+cell_size = 10  # pixels
+ms = 10  # rows and columns
 visited_cells = []
 walls = []
 revisited_cells = []
 
 # creates a list with 50 x 50 "w" items
-map = [['w' for _ in range(ms)] for _ in range(ms)]
+maze_map = [['w' for _ in range(ms)] for _ in range(ms)]
 
-def  check_neighbours(ccr, ccc):
-    neighbours = [[ ccr, ccc - 1, ccr - 1, ccc - 2, ccr, ccc - 2, ccr + 1, ccc - 2, ccr - 1, ccc - 1, ccr + 1, ccc - 1 ], #left
-                [ccr, ccc + 1, ccr - 1, ccc + 2, ccr, ccc + 2, ccr + 1, ccc + 2, ccr - 1, ccc + 1, ccr + 1, ccc + 1], #right
-                [ccr - 1, ccc, ccr - 2, ccc - 1, ccr - 2, ccc, ccr - 2, ccc + 1, ccr - 1, ccc - 1, ccr - 1, ccc + 1], #top
-                [ccr + 1, ccc, ccr + 2, ccc - 1, ccr + 2, ccc, ccr + 2, ccc + 1, ccr + 1, ccc-1, ccr + 1, ccc + 1]] #bottom
 
-    visitable_neighbours = []           
-    for i in neighbours:                                                                        #find neighbours to visit
-        if i[0] > 0 and i[0] < (ms-1) and i[1] > 0 and i[1] < (ms-1):
-            if map[i[2]][i[3]] == 'P' or map[i[4]][i[5]] == 'P' or map[i[6]][i[7]] == 'P' or map[i[8]][i[9]] == 'P' or map[i[10]][i[11]] == 'P':
-                walls.append(i[0:2])                                                                                               
+def check_neighbours(ccr, ccc):
+    neighbours = [
+        [ccr, ccc - 1, ccr - 1, ccc - 2, ccr, ccc - 2, ccr + 1, ccc - 2, ccr - 1, ccc - 1, ccr + 1, ccc - 1],  # left
+        [ccr, ccc + 1, ccr - 1, ccc + 2, ccr, ccc + 2, ccr + 1, ccc + 2, ccr - 1, ccc + 1, ccr + 1, ccc + 1],  # right
+        [ccr - 1, ccc, ccr - 2, ccc - 1, ccr - 2, ccc, ccr - 2, ccc + 1, ccr - 1, ccc - 1, ccr - 1, ccc + 1],  # top
+        [ccr + 1, ccc, ccr + 2, ccc - 1, ccr + 2, ccc, ccr + 2, ccc + 1, ccr + 1, ccc - 1, ccr + 1, ccc + 1]]  # bottom
+
+    visitable_neighbours = []
+    for i in neighbours:  # find neighbours to visit
+        if 0 < i[0] < (ms - 1) and 0 < i[1] < (ms - 1):
+            if maze_map[i[2]][i[3]] == 'P' or maze_map[i[4]][i[5]] == 'P' or maze_map[i[6]][i[7]] == 'P' or \
+                    maze_map[i[8]][i[9]] == 'P' or maze_map[i[10]][i[11]] == 'P':
+                walls.append(i[0:2])
             else:
                 visitable_neighbours.append(i[0:2])
     return visitable_neighbours
 
-#StartingPoint
+
+# StartingPoint
 
 # starting color of row
-scr = randint(1, ms)
+scr = randint(1, ms - 2)  # Adjusted range to fit within maze bounds
 # starting random column
-scc = randint(1, ms)
+scc = randint(1, ms - 2)  # Adjusted range to fit within maze bounds
 start_color = 'Green'
 # memorize row and column of the starting rectangle
 # current color row and current color column
@@ -42,14 +48,14 @@ y1 = ccc * 12
 print(scr, scc)
 print(ccr, ccc)
 
-map[ccr][ccc] = 'P'
+maze_map[ccr][ccc] = 'P'
 loop = 1
 while loop:
     visitable_neighbours = check_neighbours(ccr, ccc)
     if len(visitable_neighbours) != 0:
-        d = randint(1, len(visitable_neighbours))-1
+        d = randint(1, len(visitable_neighbours)) - 1
         ncr, ncc = visitable_neighbours[d]
-        map[ncr][ncc] = 'P'
+        maze_map[ncr][ncc] = 'P'
         visited_cells.append([ncr, ncc])
         ccr, ccc = ncr, ncc
     if len(visitable_neighbours) == 0:
@@ -60,34 +66,35 @@ while loop:
         except:
             loop = 0
 
-
-canvas_size = ms*cell_size
-y1 = scr * cell_size 
+canvas_size = ms * cell_size
+y1 = scr * cell_size
 x1 = scc * cell_size
-e = randint(1, len(revisited_cells))-1
+e = randint(1, len(revisited_cells)) - 1
 ecr = revisited_cells[e][0]
 ecc = revisited_cells[e][1]
 end_color = 'red'
 
-m = { 
-     "ms" :ms, 
-     "map" :map,
-     "cell_size": cell_size,
-     "scr": scr,
-     "scc": scc,
-     "start_color": start_color,
-     "ecr": ecr,
-     "ecc":ecc,
-     "end_color": end_color,
-     "canvas_size": canvas_size,
-     "x1": x1,
-     "y1":y1
-    }
+m = {
+    "ms": ms,
+    "map": maze_map,
+    "cell_size": cell_size,
+    "scr": scr,
+    "scc": scc,
+    "start_color": start_color,
+    "ecr": ecr,
+    "ecc": ecc,
+    "end_color": end_color,
+    "canvas_size": canvas_size,
+    "x1": x1,
+    "y1": y1
+}
+
 
 def send_msg(sock, msg):
     # Prefix each message with a 4-byte length (network byte order)
     msg = struct.pack('>I', len(msg)) + msg
     sock.sendall(msg)
+
 
 def recvall(sock, n):
     # Helper function to recv n bytes or return None if EOF is hit
@@ -99,6 +106,7 @@ def recvall(sock, n):
         data.extend(packet)
     return data
 
+
 def recv_msg(sock):
     # Read message length and unpack it into an integer
     raw_msglen = recvall(sock, 4)
@@ -108,35 +116,49 @@ def recv_msg(sock):
     # Read the message data
     return recvall(sock, msglen)
 
-def serve(m):    
+
+def serve(m):
+    # Create a TCP/IP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print ("Socket successfully created")
-     
-    port = 3423               
-     
-    s.bind(('', port))         
-    print ("socket binded to %s" %(port)) 
+    print("Socket successfully created")
+
+    # SSL context creation
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile="/Users/windows/projects/Socket-Maze-Game/server.crt", keyfile="/Users/windows/projects/Socket-Maze-Game/server.key")
+
+    port = 3423
+
+    s.bind(('', port))
+    print("socket binded to %s" % (port))
 
     while True:
+        # Put the socket into listening mode
+        s.listen(5)
+        print("socket is listening")
 
-        # put the socket into listening mode 
-        s.listen(5)     
-        print ("socket is listening")            
+        # Wait for a connection
+        c, addr = s.accept()
+        print('Got connection from', addr)
 
+        # Wrap the socket with SSL
+        ssl_sock = context.wrap_socket(c, server_side=True)
+        print("successful SSL handshake and connection establishment")
+
+        # JSON serialization and sending
         jsonObj = json.dumps(m, indent=2).encode("utf-8")
-
         data = jsonObj
-
-        c, addr = s.accept()     
-        print ('Got connection from', addr )
-
-        send_msg(c, data)
+        send_msg(ssl_sock, data)
         print("sent data")
 
-        received = recv_msg(c)
+        # Receiving data
+        received = recv_msg(ssl_sock)
         m = received.decode("utf-8")
-        timeTaken= json.loads(m)
+        timeTaken = json.loads(m)
 
-        print("Total time taken: ",timeTaken["total_time"])
+        print("Total time taken: ", timeTaken["total_time"])
+
+        # Close the SSL socket
+        ssl_sock.close()
+
 
 serve(m)
